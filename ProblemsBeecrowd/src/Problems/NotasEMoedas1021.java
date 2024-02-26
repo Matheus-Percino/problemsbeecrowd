0bdee9cc06fa72df9176d8f897ef4328e63d1e65
Matheus Percino da Silva <matheuspercinodasilva@gmail.com>
2024-02-26 19:08:56 -0300

    Exercicios feitos em 26/02/2024

--- ProblemsBeecrowd/src/Problems/NotasEMoedas1021.java
+++ ProblemsBeecrowd/src/Problems/NotasEMoedas1021.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class NotasEMoedas1021 {
 
 	public static void main(String[] args) {
-		
+		//commit
 		Scanner scanner = new Scanner(System.in);
 		Double reais = scanner.nextDouble();
 		double centavo = ((reais%100%50%20%10%5%2%1%0.50%0.25%0.10%0.05/0.01)+0.001);
