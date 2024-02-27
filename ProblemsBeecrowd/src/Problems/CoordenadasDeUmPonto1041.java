c0cd3dbb73c673076f2f3898ec3f6b785a9efd80
Matheus Percino da Silva <matheuspercinodasilva@gmail.com>
2024-02-27 19:11:31 -0300

    Exercicios feitos em 27/02/2024

---
+++ ProblemsBeecrowd/src/Problems/CoordenadasDeUmPonto1041.java
@@ -0,0 +1,39 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class CoordenadasDeUmPonto1041 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner =  new Scanner(System.in);
+		double x, y;
+		x = scanner.nextDouble();
+		y = scanner.nextDouble();
+		if(x > 0) {
+			if(y > 0) {
+				System.out.println("Q1");
+			}
+		}
+		if(x > 0) {
+			if(y < 0) {
+				System.out.println("Q4");
+			}
+		}
+		if(x < 0) {
+			if(y > 0) {
+				System.out.println("Q2");
+			}
+		}
+		if(x < 0) {
+			if(y < 0) {
+				System.out.println("Q3");
+			}
+		}
+		if(x == 0 && y ==0) {
+			System.out.println("Origem");
+		}
+	}
+
+}
